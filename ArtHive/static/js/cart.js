5492ae0f2fe501a1d6fd2f56721fbd8310178e7b
guadaaaaa <obandoguadalue951@gmail.com>
2024-11-03 23:54:40 +0800

    Adding and Removing Korek!

--- ArtHive/static/js/cart.js
+++ ArtHive/static/js/cart.js
@@ -1,18 +1,45 @@
-console.log('Hello World!')
-var updateBtns= document.getElementsByClassName('update-cart')
+document.addEventListener('DOMContentLoaded', function() {
+    if (window.hasCartLoaded) return;
+    window.hasCartLoaded = true;
+    var updateBtns = document.getElementsByClassName('add-btn');
 
-for(var i=0; i < updateBtns.length; i++) {
-    console.log('Hello World')
-    updateBtns[i].addEventListener('click', function (){
-        var productId = this.dataset.product
-        var action = this.dataset.action
-        console.log('productId:', productId, 'Action:', action)
-
-        console.log('USER:', user)
-        if(user == 'AnonymousUser'){
-            console.log('User is not Authenticated')
-        } else {
-            console.log('User is authenticated, sending data...')
+    if (updateBtns.length > 0) {
+        console.log('updateBtns is not null and has items');
+        for (var i = 0; i < updateBtns.length; i++) {
+            updateBtns[i].addEventListener('click', function () {
+                var productId = this.dataset.product;
+                var action = this.dataset.action;
+                console.log('productId:', productId, 'Action:', action);
+                console.log('USER:', user)
+                if(user === 'AnonymousUser'){
+                    console.log('User is not Authenticated')
+                } else {
+                    updateUserOrder(productId,action)
+                }
+            });
         }
-    })
-}
+    } else {
+        console.log('No update buttons found (updateBtns is null or empty).')
+    }
+    function updateUserOrder(productId, action){
+        console.log('User is authenticated, sending data...')
+        var url = '/update_item/'
+
+        fetch(url, {
+            method: 'POST',
+            headers:{
+                'Content-Type':'application/json',
+                'X-CSRFToken':csrftoken,
+            },
+            body:JSON.stringify({'productId':productId, 'action':action})
+        })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log('Data:', data)
+                location.reload()
+            })
+    }
+});
+

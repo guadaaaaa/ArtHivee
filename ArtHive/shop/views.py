3b89529f47013b867da79e7603f1c7e94a063afe
guadaaaaa <obandoguadalue951@gmail.com>
2024-11-05 14:53:30 +0800

    with update cart

--- ArtHive/shop/views.py
+++ ArtHive/shop/views.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -46,7 +47,7 @@ def checkout(request):
         order = {'get_cart_total': 0, 'get_cart_items': 0}
         cartItems = order['get_cart_items']
 
-    context = {'items': items, 'order': order}
+    context = {'items': items, 'order': order, 'cartItems':cartItems}
     return render(request, 'shop/checkout.html', context)
 
 def updateItem(request):
@@ -72,3 +73,31 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = (data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == float(order.get_cart_total):
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address=data['shipping']['address'],
+                city=data['shipping']['city'],
+                zipcode=data['shipping']['zipcode'],
+                country = data['shipping']['country'],
+            )
+
+    else:
+        print('User not logged in')
+    return JsonResponse('Payment submitted..', safe=False)
